3c7ce0fd2987d5302a9b8a8ef3e91cf12a4b9530
Hammad Haleem <hammadhaleem@gmail.com>
2015-05-04 23:32:24 +0800

    Initialized the code islip

--- code/Fabric_Class.py
+++ code/Fabric_Class.py
@@ -14,7 +14,7 @@ class RoundRobinScheduler(StandardScheduler):
 		self.input_queue_object.insert_data_in_queues(data)
 		self.pointer = self.create_state_variable()
 
-	def generate_sequences_robin(self):
+	def generate_sequences(self):
 		number_of_queue = self.number_of_queue
 		input_status =  self.Status()
 		pointer = self.pointer
@@ -75,4 +75,84 @@ class RoundRobinScheduler(StandardScheduler):
 		for i in actions:
 			self.Packet_Exchange(i[0],i[1])
 		
+		return True
+
+
+class iSlipScheduler(StandardScheduler):
+
+	def __init__(self, number_of_queue,data):
+		super(StandardScheduler, self).__init__()
+		self.number_of_queue = number_of_queue
+		self.input_queue_object  = SuperMultiQueue(int(self.number_of_queue))
+		self.output_queue_object = SuperMultiQueue(int(self.number_of_queue))
+		self.input_queue_object.insert_data_in_queues(data)
+		self.pointer = self.create_state_variable()
+
+	def islip_convert_actions(self, action_list):
+		return action_list
+
+	def generate_sequences(self):
+		self.pointer = self.create_state_variable()
+		# generate a new pointer every time
+		# Not carring the old pointer.
+		
+		number_of_queue = self.number_of_queue
+		input_status =  self.Status()
+		pointer = self.pointer
+
+		# Request for connections 
+		
+		data = {}
+		for keys in input_status :
+			obj = input_status[keys]
+			data[keys] = []
+			for ob in obj:
+				if obj[ob] > 0 :
+					data[keys].append(ob)
+		input_status = data
+		self.pointer = pointer
+
+		#Debug
+		# pp.pprint (self.pointer)
+		# pp.pprint (input_status)
+
+		#Grant sequences.
+		actions =[]
+		allocated = []
+		for keys in self.pointer:
+			if self.pointer[keys]['pointer'] is None:
+				for out in  input_status[keys]:
+						try:
+							if out not in allocated:
+								self.pointer[keys]['pointer'] = out
+								self.pointer[keys]['count'] = self.pointer[keys]['count'] - 1
+								allocated.append(out)
+								actions.append([(keys,out),out])
+								del input_status[keys][input_status[keys].index(out)]
+						except :
+							pass 
+			elif self.pointer[keys]['pointer'] is not None:
+				if (len(input_status[keys]) > 0):
+					pin = self.pointer[keys]['pointer'] +1
+					while  True:
+						if pin in input_status[keys]:
+							self.pointer[keys]['count'] = self.pointer[keys]['count'] - 1
+							allocated.append(pin)
+							actions.append([(keys,pin),pin])
+							del input_status[keys][input_status[keys].index(pin)]
+							break
+						self.pointer[keys]['pointer'] = self.pointer[keys]['pointer'] + 1
+						if self.pointer[keys]['pointer']  > number_of_queue:
+							self.pointer[keys]['pointer']  = 0 
+							pin = self.pointer[keys]['pointer']
+						else:
+							pin = self.pointer[keys]['pointer'] +1
+
+		if len(actions) < 1 :
+			return False
+			
+		actions = self.islip_convert_actions(actions)
+		print "Exchange: ",actions
+		for i in actions:
+			self.Packet_Exchange(i[0],i[1])
 		return True
--- code/main.py
+++ code/main.py
@@ -1,55 +1,54 @@
 from Queue_Class import *
 from Scheduler_Class import *
 from Fabric_Class import *
+import pprint
 
-NUMBER_OF_ACTIVE_QUEUES = 400
+pp = pprint.PrettyPrinter(indent=4)
+NUMBER_OF_ACTIVE_QUEUES = 5
+NUMBER_OF_PACKETS = 5000
+SCHEDULER_TYPE = 1 # 0 -> RR , 1-> Islip
 
 def Main(data):
+	if SCHEDULER_TYPE is 0:
+		S = RoundRobinScheduler(NUMBER_OF_ACTIVE_QUEUES,data)
+	if SCHEDULER_TYPE is 1:
+		S = iSlipScheduler(NUMBER_OF_ACTIVE_QUEUES,data)
+	while True:
+	 if S.generate_sequences() == False:
+	 	break
+	return S.Get_Output()
 
-	S = RoundRobinScheduler(NUMBER_OF_ACTIVE_QUEUES,data)
-
-	S.generate_sequences_robin()
-	print S.Status()
-	S.generate_sequences_robin()
-	print S.Status()
-
-	# S.Packet_Exchange((1,1),1)
-	# S.Packet_Exchange((1,0),0)
-	# S.Packet_Exchange((0,1),1)
-	# S.Packet_Exchange((0,0),0)
+def generate_data():
+	data_list = []
 	
-	# S.generate_sequences_robin()
-	# S.generate_sequences_robin()
-	# S.generate_sequences_robin()
-	# S.generate_sequences_robin()
-	# S.generate_sequences_robin()
+	# data_list.append({'timestamp' : time.time(),'outport' : 0,'data' : "this is my data", 'source' : 0})
+	# data_list.append({'timestamp' : time.time(),'outport' : 0,'data' : "this is my data", 'source' : 1})
+	# data_list.append({'timestamp' : time.time(),'outport' : 0,'data' : "this is my data", 'source' : 2})
+	# data_list.append({'timestamp' : time.time(),'outport' : 0,'data' : "this is my data", 'source' : 3})
 
-	#print "\nOutput : ",S.Get_Output()
+	# data_list.append({'timestamp' : time.time(),'outport' : 1,'data' : "this is my data", 'source' : 0})
+	# data_list.append({'timestamp' : time.time(),'outport' : 1,'data' : "this is my data", 'source' : 1})
+	# data_list.append({'timestamp' : time.time(),'outport' : 1,'data' : "this is my data", 'source' : 2})
+	# data_list.append({'timestamp' : time.time(),'outport' : 4,'data' : "this is my data", 'source' : 4})
 
-data_list = []
+	# data_list.append({'timestamp' : time.time(),'outport' : 2,'data' : "this is my data", 'source' : 0})
+	# data_list.append({'timestamp' : time.time(),'outport' : 2,'data' : "this is my data", 'source' : 1})
+	# data_list.append({'timestamp' : time.time(),'outport' : 2,'data' : "this is my data", 'source' : 2})
+	# data_list.append({'timestamp' : time.time(),'outport' : 2,'data' : "this is my data", 'source' : 3})
 
-# data_list.append({'timestamp' : time.time(),'outport' : 0,'data' : "this is my data", 'source' : 0})
-# data_list.append({'timestamp' : time.time(),'outport' : 0,'data' : "this is my data", 'source' : 1})
-# data_list.append({'timestamp' : time.time(),'outport' : 0,'data' : "this is my data", 'source' : 2})
-# data_list.append({'timestamp' : time.time(),'outport' : 0,'data' : "this is my data", 'source' : 3})
+	# data_list.append({'timestamp' : time.time(),'outport' : 3,'data' : "this is my data", 'source' : 0})
+	# data_list.append({'timestamp' : time.time(),'outport' : 3,'data' : "this is my data", 'source' : 1})
+	# data_list.append({'timestamp' : time.time(),'outport' : 3,'data' : "this is my data", 'source' : 2})
+	# data_list.append({'timestamp' : time.time(),'outport' : 3,'data' : "this is my data", 'source' : 3})
+	
+	data_list.append({'timestamp' : time.time(),'outport' : 1 ,'data' : "this is my data", 'source' : 1})
+	data_list.append({'timestamp' : time.time(),'outport' : 1 ,'data' : "this is my data", 'source' : 0})
+	data_list.append({'timestamp' : time.time(),'outport' : 0 ,'data' : "this is my data", 'source' : 0})
+	data_list.append({'timestamp' : time.time(),'outport' : 0 ,'data' : "this is my data", 'source' : 1})
 
-# data_list.append({'timestamp' : time.time(),'outport' : 1,'data' : "this is my data", 'source' : 0})
-# data_list.append({'timestamp' : time.time(),'outport' : 1,'data' : "this is my data", 'source' : 1})
-# data_list.append({'timestamp' : time.time(),'outport' : 1,'data' : "this is my data", 'source' : 2})
-# data_list.append({'timestamp' : time.time(),'outport' : 4,'data' : "this is my data", 'source' : 4})
+	return data_list
 
-# data_list.append({'timestamp' : time.time(),'outport' : 2,'data' : "this is my data", 'source' : 0})
-# data_list.append({'timestamp' : time.time(),'outport' : 2,'data' : "this is my data", 'source' : 1})
-# data_list.append({'timestamp' : time.time(),'outport' : 2,'data' : "this is my data", 'source' : 2})
-# data_list.append({'timestamp' : time.time(),'outport' : 2,'data' : "this is my data", 'source' : 3})
-
-# data_list.append({'timestamp' : time.time(),'outport' : 3,'data' : "this is my data", 'source' : 0})
-# data_list.append({'timestamp' : time.time(),'outport' : 3,'data' : "this is my data", 'source' : 1})
-# data_list.append({'timestamp' : time.time(),'outport' : 3,'data' : "this is my data", 'source' : 2})
-# data_list.append({'timestamp' : time.time(),'outport' : 3,'data' : "this is my data", 'source' : 3})
-
-data_list.append({'timestamp' : time.time(),'outport' : 1 ,'data' : "this is my data", 'source' : 1})
-data_list.append({'timestamp' : time.time(),'outport' : 1 ,'data' : "this is my data", 'source' : 0})
-data_list.append({'timestamp' : time.time(),'outport' : 0 ,'data' : "this is my data", 'source' : 0})
-data_list.append({'timestamp' : time.time(),'outport' : 0 ,'data' : "this is my data", 'source' : 1})
-Main(data_list)
+input_Data =  generate_data()
+print "\nInput  :\n",input_Data,"\n"
+data_list = Main(input_Data)
+print "\nOutput :\n",data_list,"\n"
