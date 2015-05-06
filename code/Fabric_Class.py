aac0f086f475a6d91682c586e391cc4845590a20
Hammad Haleem <hammadhaleem@gmail.com>
2015-05-06 21:33:20 +0800

    Finalized version with analysis.

--- code/Fabric_Class.py
+++ code/Fabric_Class.py
@@ -12,6 +12,7 @@ class RoundRobinScheduler(StandardScheduler):
 		self.input_queue_object  = SuperMultiQueue(int(self.number_of_queue))
 		self.output_queue_object = SuperMultiQueue(int(self.number_of_queue))
 		self.input_queue_object.insert_data_in_queues(data)
+
 		self.pointer = self.create_state_variable()
 
 	def generate_sequences(self):
@@ -28,16 +29,21 @@ class RoundRobinScheduler(StandardScheduler):
 				if obj[ob] > 0 :
 					data[keys].append(ob)
 		input_status = data
-		self.pointer = pointer
 
 		#Debug
 		# pp.pprint (self.pointer)
 		# pp.pprint (input_status)
 
+		# print "\n"
+
 		#Grant
 		actions =[]
 		allocated = []
+		count = 0
 		for keys in self.pointer:
+			
+
+
 			if self.pointer[keys]['pointer'] is None:
 				for out in  input_status[keys]:
 						try:
@@ -49,34 +55,54 @@ class RoundRobinScheduler(StandardScheduler):
 								del input_status[keys][input_status[keys].index(out)]
 						except :
 							pass 
-			elif self.pointer[keys]['pointer'] is not None:
 
+
+			elif self.pointer[keys]['pointer'] is not None:
+				# pp.pprint (pointer)
 				if (len(input_status[keys]) > 0):
 					pin = self.pointer[keys]['pointer'] +1
-					while  True:
+					while  True:						
 						if pin in input_status[keys]:
 							self.pointer[keys]['count'] = self.pointer[keys]['count'] - 1
 							allocated.append(pin)
 							actions.append([(keys,pin),pin])
 							del input_status[keys][input_status[keys].index(pin)]
+							# print pin ,input_status[keys] ,self.pointer[keys]['count']
 							break
 
-						self.pointer[keys]['pointer'] = self.pointer[keys]['pointer'] + 1
-						if self.pointer[keys]['pointer']  > number_of_queue:
-							self.pointer[keys]['pointer']  = 0 
-							pin = self.pointer[keys]['pointer']
 						else:
-							pin = self.pointer[keys]['pointer'] +1
+							if self.pointer[keys]['pointer']  > number_of_queue:
+								self.pointer[keys]['pointer']  = 0 
+								pin = self.pointer[keys]['pointer'] 
+							else:
+								self.pointer[keys]['pointer'] = self.pointer[keys]['pointer'] + 1
+								pin = self.pointer[keys]['pointer']  
+							# print input_status[keys], pin
+							# print pin ,input_status[keys] ,self.pointer[keys]['count']
+						# if count > 10:
+						# 	exit()
+						# count = count + 1
+						
+
 
 		if len(actions) < 1 :
 			return False
-		pp.pprint(actions)
-
 		for i in actions:
 			self.Packet_Exchange(i[0],i[1])
-		
 		return True
 
+	def create_state_variable(self):
+		states = {}
+		data =self.Status()
+		for cards in data : 
+			obj = {}
+			obj['pointer'] = None
+			obj['count'] = 0
+			obj['max'] = self.number_of_queue
+			for keys in data[cards] :
+				obj['count'] = obj['count'] +  data[cards][keys]
+			states[cards] =obj
+		return states
 
 class iSlipScheduler(StandardScheduler):
 
@@ -142,7 +168,7 @@ class iSlipScheduler(StandardScheduler):
 							del input_status[keys][input_status[keys].index(pin)]
 							break
 						self.pointer[keys]['pointer'] = self.pointer[keys]['pointer'] + 1
-						if self.pointer[keys]['pointer']  > number_of_queue:
+						if saelf.pointer[keys]['pointer']  > number_of_queue:
 							self.pointer[keys]['pointer']  = 0 
 							pin = self.pointer[keys]['pointer']
 						else:
@@ -152,7 +178,7 @@ class iSlipScheduler(StandardScheduler):
 			return False
 			
 		actions = self.islip_convert_actions(actions)
-		print "Exchange: ",actions
+		# print actions
 		for i in actions:
 			self.Packet_Exchange(i[0],i[1])
 		return True
--- code/main.py
+++ code/main.py
@@ -2,53 +2,82 @@ from Queue_Class import *
 from Scheduler_Class import *
 from Fabric_Class import *
 import pprint
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
 
 pp = pprint.PrettyPrinter(indent=4)
-NUMBER_OF_ACTIVE_QUEUES = 5
+NUMBER_OF_ACTIVE_QUEUES = 6
 NUMBER_OF_PACKETS = 5000
-SCHEDULER_TYPE = 1 # 0 -> RR , 1-> Islip
+SCHEDULER_TYPE = 0 # 0 -> RR , 1-> Islip
+
+
+#########Configration goes above this line ######################
+NUMBER_OF_ACTIVE_QUEUES = NUMBER_OF_ACTIVE_QUEUES + 1
+
 
 def Main(data):
-	if SCHEDULER_TYPE is 0:
-		S = RoundRobinScheduler(NUMBER_OF_ACTIVE_QUEUES,data)
-	if SCHEDULER_TYPE is 1:
-		S = iSlipScheduler(NUMBER_OF_ACTIVE_QUEUES,data)
-	while True:
-	 if S.generate_sequences() == False:
-	 	break
-	return S.Get_Output()
+	if SCHEDULER_TYPE == 0:	
+		S_1 = RoundRobinScheduler(NUMBER_OF_ACTIVE_QUEUES,data)
+		while S_1.generate_sequences():
+		 	pass
+		return S_1.Get_Output()
+	elif SCHEDULER_TYPE == 1:
+		S_2 = iSlipScheduler(NUMBER_OF_ACTIVE_QUEUES,data)
+		while S_2.generate_sequences() :
+		 	pass
+		return S_2.Get_Output()
 
-def generate_data():
+def generate_uniform_traffic(number_of_cards,packets=1):
 	data_list = []
-	
-	# data_list.append({'timestamp' : time.time(),'outport' : 0,'data' : "this is my data", 'source' : 0})
-	# data_list.append({'timestamp' : time.time(),'outport' : 0,'data' : "this is my data", 'source' : 1})
-	# data_list.append({'timestamp' : time.time(),'outport' : 0,'data' : "this is my data", 'source' : 2})
-	# data_list.append({'timestamp' : time.time(),'outport' : 0,'data' : "this is my data", 'source' : 3})
-
-	# data_list.append({'timestamp' : time.time(),'outport' : 1,'data' : "this is my data", 'source' : 0})
-	# data_list.append({'timestamp' : time.time(),'outport' : 1,'data' : "this is my data", 'source' : 1})
-	# data_list.append({'timestamp' : time.time(),'outport' : 1,'data' : "this is my data", 'source' : 2})
-	# data_list.append({'timestamp' : time.time(),'outport' : 4,'data' : "this is my data", 'source' : 4})
-
-	# data_list.append({'timestamp' : time.time(),'outport' : 2,'data' : "this is my data", 'source' : 0})
-	# data_list.append({'timestamp' : time.time(),'outport' : 2,'data' : "this is my data", 'source' : 1})
-	# data_list.append({'timestamp' : time.time(),'outport' : 2,'data' : "this is my data", 'source' : 2})
-	# data_list.append({'timestamp' : time.time(),'outport' : 2,'data' : "this is my data", 'source' : 3})
-
-	# data_list.append({'timestamp' : time.time(),'outport' : 3,'data' : "this is my data", 'source' : 0})
-	# data_list.append({'timestamp' : time.time(),'outport' : 3,'data' : "this is my data", 'source' : 1})
-	# data_list.append({'timestamp' : time.time(),'outport' : 3,'data' : "this is my data", 'source' : 2})
-	# data_list.append({'timestamp' : time.time(),'outport' : 3,'data' : "this is my data", 'source' : 3})
-	
-	data_list.append({'timestamp' : time.time(),'outport' : 1 ,'data' : "this is my data", 'source' : 1})
-	data_list.append({'timestamp' : time.time(),'outport' : 1 ,'data' : "this is my data", 'source' : 0})
-	data_list.append({'timestamp' : time.time(),'outport' : 0 ,'data' : "this is my data", 'source' : 0})
-	data_list.append({'timestamp' : time.time(),'outport' : 0 ,'data' : "this is my data", 'source' : 1})
-
+	for m in range(packets):
+		for i in range(0,number_of_cards):
+			for j in range(0,number_of_cards):
+				obj = {'timestamp' : time.time(),'outport' : i ,'data' : "this is my data", 'source' : j}
+				data_list.append(obj)
 	return data_list
 
-input_Data =  generate_data()
-print "\nInput  :\n",input_Data,"\n"
-data_list = Main(input_Data)
-print "\nOutput :\n",data_list,"\n"
+def generate_bursty_traffic(number_of_cards, number_of_active_outputs,packets=1):
+	data_list = []
+	for m in range(packets):
+		for j in xrange(0,number_of_cards):
+			for i in xrange(0,number_of_active_outputs):
+				obj = {'timestamp' : time.time(),'outport' : i ,'data' : "this is my data", 'source' : j}
+				data_list.append(obj)
+	return data_list
+
+def get_average_time(data_list):
+	number = 0.0
+	for obj in data_list:
+		number = number + obj['time_out'] - obj['timestamp'] 
+	return number, len(data_list)
+
+def run_analysis(multiplier,type_in=0):
+	if type_in == 0 :
+		input_data =  generate_uniform_traffic(5,multiplier)
+	else:
+		input_data =  generate_bursty_traffic(5,2,multiplier) # sources are bursty 
+	data_list = Main(input_data)
+	sum_of_diff, number = get_average_time(data_list)
+	return (sum_of_diff/number) , number
+
+
+pairs = []
+plt1 =plt
+for multiplier in xrange(1,75):
+	avg,number = run_analysis(multiplier,1)
+	print multiplier,avg,number
+	pairs.append((number,avg))
+
+plt1.plot([p[0] for p in pairs], [p[1] for p in pairs], '.')
+
+pairs = []
+for multiplier in xrange(1,75):
+	avg,number = run_analysis(multiplier,0)
+	print multiplier,avg,number
+	pairs.append((number,avg))
+
+plt.plot([p[0] for p in pairs], [p[1] for p in pairs], '.')
+plt1.show()
+plt.show()
+
